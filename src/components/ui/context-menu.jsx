b8116cf4c142192349cc63588e3dc505998c1fba
apeterson808 <petersonandrew@hotmail.com>
2025-12-17 10:35:07 -0700

    Updated App.jsx

--- src/components/ui/context-menu.jsx
+++ src/components/ui/context-menu.jsx
@@ -31,56 +31,28 @@ const ContextMenuSubTrigger = React.forwardRef(({ className, inset, children, ..
 ))
 ContextMenuSubTrigger.displayName = ContextMenuPrimitive.SubTrigger.displayName
 
-const ContextMenuSubContent = React.forwardRef(({ className, ...props }, ref) => {
-  const stopEvent = (e) => e.stopPropagation()
+const ContextMenuSubContent = React.forwardRef(({ className, ...props }, ref) => (
+  <ContextMenuPrimitive.SubContent
+    ref={ref}
+    className={cn(
+      "z-50 min-w-[8rem] overflow-hidden rounded-md border bg-popover p-1 text-popover-foreground shadow-lg data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
+      className
+    )}
+    {...props} />
+))
+ContextMenuSubContent.displayName = ContextMenuPrimitive.SubContent.displayName
 
-  return (
-    <ContextMenuPrimitive.SubContent
+const ContextMenuContent = React.forwardRef(({ className, ...props }, ref) => (
+  <ContextMenuPrimitive.Portal>
+    <ContextMenuPrimitive.Content
       ref={ref}
-      onClick={stopEvent}
-      onMouseDown={stopEvent}
-      onMouseUp={stopEvent}
-      onDoubleClick={stopEvent}
-      onKeyDown={stopEvent}
-      onKeyUp={stopEvent}
-      onFocus={stopEvent}
-      onBlur={stopEvent}
-      onPointerDown={stopEvent}
-      onPointerUp={stopEvent}
       className={cn(
-        "z-50 min-w-[8rem] overflow-hidden rounded-md border bg-popover p-1 text-popover-foreground shadow-lg data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
+        "z-50 min-w-[8rem] overflow-hidden rounded-md border bg-popover p-1 text-popover-foreground shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
         className
       )}
       {...props} />
-  )
-})
-ContextMenuSubContent.displayName = ContextMenuPrimitive.SubContent.displayName
-
-const ContextMenuContent = React.forwardRef(({ className, ...props }, ref) => {
-  const stopEvent = (e) => e.stopPropagation()
-
-  return (
-    <ContextMenuPrimitive.Portal>
-      <ContextMenuPrimitive.Content
-        ref={ref}
-        onClick={stopEvent}
-        onMouseDown={stopEvent}
-        onMouseUp={stopEvent}
-        onDoubleClick={stopEvent}
-        onKeyDown={stopEvent}
-        onKeyUp={stopEvent}
-        onFocus={stopEvent}
-        onBlur={stopEvent}
-        onPointerDown={stopEvent}
-        onPointerUp={stopEvent}
-        className={cn(
-          "z-50 min-w-[8rem] overflow-hidden rounded-md border bg-popover p-1 text-popover-foreground shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
-          className
-        )}
-        {...props} />
-    </ContextMenuPrimitive.Portal>
-  )
-})
+  </ContextMenuPrimitive.Portal>
+))
 ContextMenuContent.displayName = ContextMenuPrimitive.Content.displayName
 
 const ContextMenuItem = React.forwardRef(({ className, inset, ...props }, ref) => (
--- src/components/ui/sheet.jsx
+++ src/components/ui/sheet.jsx
@@ -14,29 +14,15 @@ const SheetClose = SheetPrimitive.Close
 
 const SheetPortal = SheetPrimitive.Portal
 
-const SheetOverlay = React.forwardRef(({ className, ...props }, ref) => {
-  const stopEvent = (e) => e.stopPropagation()
-
-  return (
-    <SheetPrimitive.Overlay
-      onClick={stopEvent}
-      onMouseDown={stopEvent}
-      onMouseUp={stopEvent}
-      onDoubleClick={stopEvent}
-      onKeyDown={stopEvent}
-      onKeyUp={stopEvent}
-      onFocus={stopEvent}
-      onBlur={stopEvent}
-      onPointerDown={stopEvent}
-      onPointerUp={stopEvent}
-      className={cn(
-        "fixed inset-0 z-50 bg-black/80 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
-        className
-      )}
-      {...props}
-      ref={ref} />
-  )
-})
+const SheetOverlay = React.forwardRef(({ className, ...props }, ref) => (
+  <SheetPrimitive.Overlay
+    className={cn(
+      "fixed inset-0 z-50 bg-black/80 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
+      className
+    )}
+    {...props}
+    ref={ref} />
+))
 SheetOverlay.displayName = SheetPrimitive.Overlay.displayName
 
 const sheetVariants = cva(
@@ -58,38 +44,22 @@ const sheetVariants = cva(
   }
 )
 
-const SheetContent = React.forwardRef(({ side = "right", className, children, ...props }, ref) => {
-  const stopEvent = (e) => e.stopPropagation()
-
-  return (
-    <SheetPortal>
-      <SheetOverlay />
-      <SheetPrimitive.Content
-        ref={ref}
-        onClick={stopEvent}
-        onMouseDown={stopEvent}
-        onMouseUp={stopEvent}
-        onDoubleClick={stopEvent}
-        onKeyDown={stopEvent}
-        onKeyUp={stopEvent}
-        onFocus={stopEvent}
-        onBlur={stopEvent}
-        onPointerDown={stopEvent}
-        onPointerUp={stopEvent}
-        className={cn(sheetVariants({ side }), className)}
-        {...props}>
-        <SheetPrimitive.Close
-          onClick={stopEvent}
-          onMouseDown={stopEvent}
-          className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
-          <X className="h-4 w-4" />
-          <span className="sr-only">Close</span>
-        </SheetPrimitive.Close>
-        {children}
-      </SheetPrimitive.Content>
-    </SheetPortal>
-  )
-})
+const SheetContent = React.forwardRef(({ side = "right", className, children, ...props }, ref) => (
+  <SheetPortal>
+    <SheetOverlay />
+    <SheetPrimitive.Content
+      ref={ref}
+      className={cn(sheetVariants({ side }), className)}
+      {...props}>
+      <SheetPrimitive.Close
+        className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
+        <X className="h-4 w-4" />
+        <span className="sr-only">Close</span>
+      </SheetPrimitive.Close>
+      {children}
+    </SheetPrimitive.Content>
+  </SheetPortal>
+))
 SheetContent.displayName = SheetPrimitive.Content.displayName
 
 const SheetHeader = ({
