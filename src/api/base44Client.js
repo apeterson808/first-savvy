cc52c0bf7bd469066b49bf4904be1ca97ca41067
apeterson808 <petersonandrew@hotmail.com>
2025-12-18 14:43:52 -0700

    Updated main.jsx

--- src/api/base44Client.js
+++ src/api/base44Client.js
@@ -1,3 +1,8 @@
-import { createSupabaseClient } from './supabaseClient';
+import { createSupabaseClient, supabase } from './supabaseClient';
 
 export const base44 = createSupabaseClient();
+
+if (typeof window !== 'undefined') {
+  window.base44 = base44;
+  window.supabase = supabase;
+}
