39e07dbebdca40e26df7322f3e829a6b29143281
apeterson808 <petersonandrew@hotmail.com>
2025-12-27 16:54:35 -0700

    Updated AddBudgetItemSheet.jsx

--- src/components/budgeting/AddBudgetItemSheet.jsx
+++ src/components/budgeting/AddBudgetItemSheet.jsx
@@ -174,10 +174,10 @@ export default function AddBudgetItemSheet({
                   <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
                 </Button>
               </PopoverTrigger>
-              <PopoverContent className="w-[400px] p-0" align="start">
+              <PopoverContent className="w-[var(--radix-popover-trigger-width)] p-0" align="start">
                 <Command>
                   <CommandInput placeholder="Search categories..." />
-                  <CommandList>
+                  <CommandList className="max-h-[300px]">
                     <CommandEmpty>No category found.</CommandEmpty>
                     <CommandGroup>
                       {availableCategories.map((cat) => (
