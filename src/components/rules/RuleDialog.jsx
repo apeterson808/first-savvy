f4a442ee109c4674904267752db4d88207cd4914
Andrew Peterson <petersonandrew@hotmail.com>
2026-02-18 18:42:41 -0700

    Updated RuleDialog.jsx

--- src/components/rules/RuleDialog.jsx
+++ src/components/rules/RuleDialog.jsx
@@ -87,10 +87,10 @@ export function RuleDialog({ open, onOpenChange, mode = 'create', rule = null, t
   });
 
   const { data: existingRules = [] } = useQuery({
-    queryKey: ['transaction-rules', profileId],
+    queryKey: ['transaction-rules-manual', profileId],
     queryFn: async () => {
       const rules = await transactionRulesApi.listRules(profileId, { enabled: true });
-      return rules.filter(r => !r.created_from_transaction_id);
+      return rules.filter(r => !r.created_from_transaction_id && !r.name?.startsWith('Auto:'));
     },
     enabled: !!profileId && open && !isEditMode
   });
