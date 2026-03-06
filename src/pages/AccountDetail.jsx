a9443c7c7fc41adefec80bd3881ba87128233e02
Andrew Peterson <petersonandrew@hotmail.com>
2026-03-05 21:11:18 -0700

    Updated AccountDetail.jsx

--- src/pages/AccountDetail.jsx
+++ src/pages/AccountDetail.jsx
@@ -892,7 +892,7 @@ export default function AccountDetail() {
                   onClick={handleImport}
                   className="gap-1.5 h-8 px-2.5"
                 >
-                  <Download className="w-3.5 h-3.5" />
+                  <Upload className="w-3.5 h-3.5" />
                   Import
                 </Button>
                 <Button
@@ -901,7 +901,7 @@ export default function AccountDetail() {
                   onClick={handleExport}
                   className="gap-1.5 h-8 px-2.5"
                 >
-                  <Upload className="w-3.5 h-3.5" />
+                  <Download className="w-3.5 h-3.5" />
                   Export
                 </Button>
                 <Button
