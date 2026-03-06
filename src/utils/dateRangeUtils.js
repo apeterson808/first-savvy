693bb4f132ddad939909c0dba946a1ca5a4b48c5
Andrew Peterson <petersonandrew@hotmail.com>
2026-03-05 21:46:21 -0700

    Updated dateRangeUtils.js

--- src/utils/dateRangeUtils.js
+++ src/utils/dateRangeUtils.js
@@ -29,6 +29,42 @@ export function getDateRangeFromPreset(preset) {
         end: endOfDay(today)
       };
 
+    case 'last3months':
+      return {
+        start: startOfDay(subMonths(today, 3)),
+        end: endOfDay(today)
+      };
+
+    case 'last6months':
+      return {
+        start: startOfDay(subMonths(today, 6)),
+        end: endOfDay(today)
+      };
+
+    case 'last12months':
+      return {
+        start: startOfDay(subMonths(today, 12)),
+        end: endOfDay(today)
+      };
+
+    case 'mtd':
+      return {
+        start: startOfMonth(today),
+        end: endOfDay(today)
+      };
+
+    case 'qtd':
+      return {
+        start: startOfQuarter(today),
+        end: endOfDay(today)
+      };
+
+    case 'ytd':
+      return {
+        start: startOfYear(today),
+        end: endOfDay(today)
+      };
+
     case 'thisMonth':
       return {
         start: startOfMonth(today),
